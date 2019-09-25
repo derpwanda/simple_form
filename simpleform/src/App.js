1be1761e8c6560d972dc4f998cf04dc932d71497
derpWanda <derpWanda@gmail.com>
2019-09-24 19:13:02 -0500

    added about, home components; add to App

--- simpleform/src/App.js
+++ simpleform/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Form from "./components/form";
+import About from "./components/about";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/form'>Form</Link>
 
-      <Link to='/'>Home</Link>
-      <Link to='/form'>Form</Link>
-
-      <Route exact path="/" component={Home} />
-      <Route path="/form" component={Form} />
-
+        <Route exact path="/" component={Home} />
+        <Route path='/about' component={About} />
+        <Route path="/form" component={Form} />
+      </BrowserRouter>
     </div>
   );
 }
