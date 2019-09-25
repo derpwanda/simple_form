37ded15217a2cc5ef2f0a4628913e34aa504433b
derpWanda <derpWanda@gmail.com>
2019-09-24 20:00:00 -0500

    remove BrowserRouter

--- simpleform/src/index.js
+++ simpleform/src/index.js
@@ -4,9 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <App />,
     document.getElementById("root")
 );
 
