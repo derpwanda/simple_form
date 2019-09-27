0e2ac53e347f795a27f529781e6afe0caa79db32
derpWanda <derpWanda@gmail.com>
2019-09-26 20:33:43 -0500

    login form build. state updating.

--- simpleform/src/components/login.js
+++ simpleform/src/components/login.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
-function App() {
+function Login() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleNameChange = event => {
+        setName(event.target.value);
+    };
+
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(name);
+        console.log(password);
+    };
+
     return (
         <div className="App">
-            <form>
-                <input />
+            {console.log({ name })}
+            {console.log({ password })}
+            <form onSubmit={event => handleSubmit(event)}>
+                <label>
+                    Username:
+          <input type="text" onChange={event => handleNameChange(event)} />
+                </label>
+                <label>
+                    Password:
+          <input type="text" onChange={event => handlePasswordChange(event)} />
+                </label>
+                <button>Submit!</button>
             </form>
         </div>
     );
 }
 
-export default App;
+export default Login;
