ac1b0dc53ad66b3b17f55146f3bb07c6f6c678cd
derpWanda <derpWanda@gmail.com>
2019-09-26 20:10:44 -0500

    added App.css to components

--- simpleform/src/App.js
+++ simpleform/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import "./App.css";
 import { /* BrowserRouter, */ Route, Link } from 'react-router-dom';
 import Form from "./components/form";
 import About from "./components/about";
 import Home from "./components/home";
+import Login from "./components/login";
 
 function App() {
   return (
@@ -12,12 +14,14 @@ function App() {
           <li><Link to='/about'>About</Link></li>
           <li><Link to='/form'>Form</Link></li>
           <li><Link to='/'>Home</Link></li>
+          <li><Link to='/login'>Login</Link></li>
         </ul>
       </div>
 
       <hr />
 
       <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
       <Route path='/about' component={About} />
       <Route path="/form" component={Form} />
     </div>
